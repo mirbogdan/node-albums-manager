da180bcaac270abf0de53d913c3fb65f708d80ce
BogdanM <bogdan.mireanu@endava.com>
2022-12-12 17:28:11 +0200

    some clean up

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const app = express();
 app.use(express.json());
 
 //routes
-app.get("/hello", (req, res) => {
-  res.send("<h1>Task manager app</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>Vinyl Albums manager app</h1>");
 });
 
-// app.use("/api/v1/tasks", tasks);
-//app.get('/api/v1/tasks')
-//app.post('/api/v1/tasks')
-//app.get('/api/v1/tasks/:id')
-//app.patch('/api/v1/tasks/:id')
-//app.delete('/api/v1/tasks/:id')
+//app.get('/api/v1/albums') - get all albums
+//app.post('/api/v1/albums') - create a new album
+//app.get('/api/v1/albums/:id') - get a single album
+//app.patch('/api/v1/albums/:id') - update album
+//app.delete('/api/v1/albums/:id') - delete a single album
 
 const port = 4000;
 app.listen(port, console.log(`server listening on port ${port}....`));
